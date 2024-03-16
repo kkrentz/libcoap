e09f35ef73c812e6aec3af6fe251c98adeb7693a
kkrentz <konrad.krentz@gmail.com>
2024-03-16 22:53:18 +0100

    Contiki-NG: Crypto adapter for OSCORE

--- src/coap_mbedtls.c
+++ src/coap_mbedtls.c
@@ -3162,7 +3162,7 @@ error:
 }
 #endif /* COAP_WS_SUPPORT */
 
-#if COAP_OSCORE_SUPPORT
+#if COAP_OSCORE_SUPPORT && !defined(WITH_CONTIKI)
 int
 coap_oscore_is_supported(void) {
   return 1;
@@ -3490,7 +3490,7 @@ error:
   return ret;
 }
 
-#endif /* COAP_OSCORE_SUPPORT */
+#endif /* COAP_OSCORE_SUPPORT && !WITH_CONTIKI */
 
 #else /* ! COAP_WITH_LIBMBEDTLS */
 
